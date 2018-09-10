b7c943d8017f4c69cd21cd2a903ae550966962fb
Antonio Carlucci <antonio.carlucci@sunrise.ch>
2018-09-10 14:22:31 +0200

    CleanC++17DesignPatternFactory impl

--- PraxisWissenC++NewStd/PraxisWissenC++NewStd/LoggerFactory.h
+++ PraxisWissenC++NewStd/PraxisWissenC++NewStd/LoggerFactory.h
@@ -14,18 +14,28 @@ private:
     enum class OutputTarget : int
     {
         STDOUT,
-        FILE
+        FILE,
+        NOCONFIG
     };
+
+    const string configFileName;
 
 public:
     explicit LoggerFactory(string configFileName) :
         configFileName {configFileName} {}
 
-    LoggerPtr create() const 
+    LoggerPtr create() const
     {
         const string configFileContent = readConfigFile();
         OutputTarget outputTarget = evalConfig(configFileContent);
-        return createLogger(outputTarget);
+        if (createLogger(outputTarget) != NULL)
+        {
+            return createLogger(outputTarget);
+        }
+        else
+        {
+            throw ("Konfigurationsdatei enthaelt keinen gueltigen Konfigurationsparameter!\n");
+        }
     }
 
 private:
@@ -38,24 +48,34 @@ private:
 
     OutputTarget evalConfig(string configFileContent) const
     {
-        // Evaluate the content of the evaluation file...
-        return OutputTarget::STDOUT;
+        if (configFileContent == "FileSystem")
+        {
+            return OutputTarget::FILE;
+        }
+        else if (configFileContent == "StandardOutput")
+        {
+            return OutputTarget::STDOUT;
+        }
+        else
+        {
+            return OutputTarget::NOCONFIG;
+        }
+
+        
     }
 
     LoggerPtr createLogger(OutputTarget outputTarget) const 
     {
         switch (outputTarget)
         {
-        case OutputTarget::FILE :
-            return make_shared<FileSystemLoggerImpl>();
-        case OutputTarget::STDOUT :
-        default:
-            return make_shared<StandardOutputLoggerImpl>();
+            case OutputTarget::FILE :
+                return make_shared<FileSystemLoggerImpl>();
+            case OutputTarget::STDOUT :
+                return make_shared<StandardOutputLoggerImpl>();
+            default:
+                return NULL;
+                
         }
     }
-
-
-
-    const string configFileName;
 };
 
